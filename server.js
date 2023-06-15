81f1d1eac59c3a13cf0f142b0e9ccbffe6645523
e341c <tabyserik@gmail.com>
2023-06-15 18:21:49 +0600

    add all pages then created folder server and add db js

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require('express')
 
 const app = express()
 
+require('./server/config/db.js')
+
 app.use(express.static(__dirname + '/public'))
 
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render("index.ejs")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs")
+})
+
+app.get('/profile', (req, res) => {
+    res.render("profile.ejs")
+})
+
+app.get('/admin', (req, res) => {
+    res.render("adminProfile.ejs")
+})
+
+app.get('/new', (req, res) => {
+    res.render("newFilm.ejs")
+})
+
+app.get('/edit', (req, res) => {
+    res.render("editFilm.ejs")
 })
 
 const PORT = 8000
